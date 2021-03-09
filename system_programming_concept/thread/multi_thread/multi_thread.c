1f122815a237598c47b2f9565b890b23e39921c9
xuanduc611 <xuanduc611@gmail.com>
2021-03-09 17:01:33 +0700

    Update multithreaded application sample

--- system_programming_concept/thread/multi_thread/multi_thread.c
+++ system_programming_concept/thread/multi_thread/multi_thread.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static int g = 0;
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
 static void *my_thread(void *arg);
 
@@ -11,21 +13,28 @@ static void *my_thread(void *arg)
     int *my_id = (int *)arg;
     static int s = 0;
 
-    ++s;
-    ++g;
+    while (1) {
+        pthread_mutex_lock(&mtx);
+        
+        printf("Thread ID = %d, Static = %d, Global = %d\n", *my_id, s++, g++);
 
-    printf("Thread ID = %d, Static = %d, Global = %d\n", *my_id, ++s, ++g);
-
+        pthread_mutex_unlock(&mtx);
+        sleep(1);
+    }
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
     int i;
-    pthread_t tid;
+    pthread_t tid[3];
 
     for (i = 0; i < 3; i++) {
-        pthread_create(&tid, NULL, my_thread, (void *)&tid);
+        pthread_create(&tid[i], NULL, my_thread, (void *)&tid[i]);
+    }
+
+    for (i = 0; i < 3; i++) {
+        pthread_join(tid[i], NULL);
     }
 
     pthread_exit(NULL);
